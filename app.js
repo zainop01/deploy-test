3f4f84809c04b1abcd3c3b8ddc1846c49f4921fd
sabtain.anwar <zainop001@gmail.com>
2024-09-27 09:13:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/' , (req , res) => {
+    res.send('Hello from the server!');
+    res.end();
+})
+
+
+app.listen(3000,
+    () => console.log('Server is running on port 3000')
+ );
